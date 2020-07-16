6f7e6442fa2a2658e181454010cf3c4c70ef47c9
xudongyss <xudong.yss@gmail.com>
2020-07-16 09:51:13 +0800

    Update QRinputItem.php

--- src/QRinputItem.php
+++ src/QRinputItem.php
@@ -119,7 +119,7 @@ class QRinputItem {
     {
         try {
             
-            $bs = new QRbitrtream();
+            $bs = new QRbitstream();
             
             $bs->appendNum(4, 0x8);
             $bs->appendNum(QRspec::lengthIndicator(QR_MODE_KANJI, $version), (int)($this->size / 2));
